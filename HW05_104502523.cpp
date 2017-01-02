70166fe9b2c05ca664c0775de3a6ecadfe81ef00
蔡富全 <chuanchan@Chuanchans-MacBook-Pro.local>
2017-01-03 00:44:40 +0800

    Finish Prim's algorithm. Fixed some typo in Makefile and filename. Added testfile to .gitignore

--- HW05_104502523.cpp
+++ HW05_104502523.cpp
@@ -62,7 +62,33 @@ void createmap(int *a)
 
 void prim(int *a)
 {
+    int ans[100] = {0}, dis[10], vis[10] = {0}, parrent[10];
     cout << "Prim's Algorithm:" << endl;
+    for(int i = 0; i < 10; i++){
+        dis[i] = 2147483647; // distance to MST
+	parrent[i] = i; // consider every node as a tree of itself; union with other tree at each selection
+    }
+    dis[0] = 0;
+    for(int j = 0; j < 10; j++){
+    	int min = 2147483647, choose = -1;
+        for(int i = 0; i < 10; i++){
+            if(!vis[i] && dis[i] < min){ // find the closest node
+	        choose = i;
+	        min = dis[i];
+	    }
+        }
+        if(choose == -1) return; // no more nodes
+        ans[parrent[choose] * 10 + choose] = dis[choose];
+	ans[choose * 10 + parrent[choose]] = dis[choose];
+        dumparr(ans);
+        vis[choose] = 1;
+        for(int i = 0; i < 10; i++){
+            if(!vis[i] && a[choose * 10 + i] && a[choose * 10 + i] < dis[i]){
+	        dis[i] = a[choose * 10 + i]; // update distance to the current MST
+	        parrent[i] = choose; // union nodes to the current MST
+	    }
+        }
+    }
 }
 
 void kruskal(int *a)
@@ -75,7 +101,7 @@ bool DFSLoop(int *a, int x)
     visited[x] = true;
     for(int i = 0; i < 10; i++){
         if(a[x * 10 + i]){
-	    if(visited[i] || DFSLoop(a, i))  return true;
+	    if(visited[i] || DFSLoop(a, i))  return true; // it is looped when visit a visited node or it's subgraph is looped
 	}
     }
     visited[x] = false;
--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-all:HW04_104502523.cpp
-	g++ -o test HW04_104502523.cpp -std=gnu++11
+all:HW05_104502523.cpp
+	g++ -o test HW05_104502523.cpp -std=gnu++11
 clean:
 	rm test
